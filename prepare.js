12ea0ce26a9cc729ab1826b1c1f72c8016d54252
Selcuk Kekec <khskekec@gmail.com>
2022-11-12 02:31:39 +0100

    Code block added to .smali

--- prepare.js
+++ prepare.js
@@ -6,12 +6,12 @@ const content = fs.readFileSync(fileName, 'utf8');
 
 const replaceLineNumbers = str => str.replaceAll(/^\s+\.line \d+$/gm, '');
 const replaceSet = {
-    'Lcom/diakem/dexcomg7broadcastboilerplate/TxCommState;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxCommState;',
-    'Lcom/diakem/dexcomg7broadcastboilerplate/TxServiceRecord;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxServiceRecord;',
-    'Lcom/diakem/dexcomg7broadcastboilerplate/SensorReading;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/SensorReading;',
-    'Lcom/diakem/dexcomg7broadcastboilerplate/GIU;': 'Lym/qj;',
-    'Lcom/diakem/dexcomg7broadcastboilerplate/TUI;': 'Lym/kFw;',
-    'Lcom/diakem/dexcomg7broadcastboilerplate/TrendArrow;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TrendArrow;'
+  'Lcom/diakem/dexcomg7broadcastboilerplate/TxCommState;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxCommState;',
+  'Lcom/diakem/dexcomg7broadcastboilerplate/TxServiceRecord;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxServiceRecord;',
+  'Lcom/diakem/dexcomg7broadcastboilerplate/SensorReading;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/SensorReading;',
+  'Lcom/diakem/dexcomg7broadcastboilerplate/GIU;': 'Lym/qj;',
+  'Lcom/diakem/dexcomg7broadcastboilerplate/TUI;': 'Lym/kFw;',
+  'Lcom/diakem/dexcomg7broadcastboilerplate/TrendArrow;': 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TrendArrow;'
 };
 
 const replaceBroadcastInvocation = str => str.replaceAll(/^\s+:try_end_0$/gm, `
@@ -28,16 +28,27 @@ const replaceBroadcastInvocation = str => str.replaceAll(/^\s+:try_end_0$/gm, `
 `);
 
 const replaceMap = [
-    {search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TxCommState;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxCommState;'},
-    {search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TxServiceRecord;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxServiceRecord;'},
-    {search: 'Lcom/diakem/dexcomg7broadcastboilerplate/SensorReading;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/SensorReading;'},
-    {search: 'Lcom/diakem/dexcomg7broadcastboilerplate/GIU;', replace: 'Lym/qj;'},
-    {search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TUI;', replace: 'Lym/kFw;'},
-    {search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TrendArrow;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TrendArrow;'},
+  { search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TxCommState;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxCommState;' },
+  { search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TxServiceRecord;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TxServiceRecord;' },
+  { search: 'Lcom/diakem/dexcomg7broadcastboilerplate/SensorReading;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/SensorReading;' },
+  { search: 'Lcom/diakem/dexcomg7broadcastboilerplate/GIU;', replace: 'Lym/qj;' },
+  { search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TUI;', replace: 'Lym/kFw;' },
+  { search: 'Lcom/diakem/dexcomg7broadcastboilerplate/TrendArrow;', replace: 'Lcom/dexcom/coresdk/g7appcore/txservice/models/TrendArrow;' },
 ];
 
 const replaceStaticReferences = str => replaceMap.reduce((acc, cur) => acc.replaceAll(cur.search, cur.replace), str);
 
 const extract = content.match(/\.method public broadcastToAAPS[\s\S]*\.end method/g)[0];
 
-fs.writeFileSync('./final-broadcasting.smali', replaceStaticReferences(replaceBroadcastInvocation(replaceLineNumbers(extract))));
+const docBlock = `
+# broadcastToAAPS()
+#
+# Broadcast sensor readings to Android APS
+#
+# @author Selcuk Kekec <khskekec@gmail.com>
+# @org DiaKEM
+# @url https://github.com/DiaKEM/dexcom-g7-apk-patcher
+# @date ${(new Date()).toISOString()}
+`;
+
+fs.writeFileSync('./final-broadcasting.smali', docBlock + (replaceBroadcastInvocation(replaceLineNumbers(extract))));
